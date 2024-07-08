7f9155b96aec38728b1a68f82572e031e548c645
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2024-07-08 10:18:29 -0300

    feat: update directory structure

--- src/com/example/views/landing.cljs
+++ src/com/example/views/landing.cljs
@@ -1,11 +1,11 @@
 (ns com.example.views.landing
   (:require
    [helix.core :refer [$ defnc <>]]
-   [helix.dom :as hd]
+   [helix.dom :as d]
    [helix.hooks :as hh]
    [refx.alpha :as refx]))
 
 (defnc landing []
-  (hd/div
+  (d/div
     {:class "bg-gray-100"}
     "Hello, World!"))
